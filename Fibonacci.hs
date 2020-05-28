68bf5591257a9bad2e2b8d683854fa149a42305d
Jacob Tan <2jacobtan@gmail.com>
2020-05-28 15:22:07 +0800

    .

--- Fibonacci.hs
+++ Fibonacci.hs
@@ -5,15 +5,15 @@ fib n = snd $ go n
     -- go :: Int -> (Int,Int)
     go 0 = (0,0)
     go 1 = (0,1)
-    go x = (b,(a+b))
+    go x = (b,a+b)
       where
         (a,b) = go (x - 1)
 
 -- tail recursive
 fib' n = go n (0,1)
   where
-    go 0 (a,b) = a
-    go 1 (a,b) = b
+    go 0 (a,_) = a
+    go 1 (_,b) = b
     go x (a,b) = go (x-1) (b,a+b)
 
 main = do
